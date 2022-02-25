44f74b40294121aabb9128093be2d4cc242fc0ad
jellyjoji <74365275+jellyjoji@users.noreply.github.com>
2022-02-26 01:19:38 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let pc_menu_list_four = document.querySelector("#four");
 // this is dark mode
 darkMode.addEventListener("click", function() {
     document.body.classList.toggle("dark_mode");
+    // i don't know how to deal w white icon...
+    // darkMode.style.backgroundImage = "url('./sun-line.svg')";
 })
 // this is pc_menu_list
 function pc_menu_click_1() {
@@ -52,4 +54,47 @@ function pc_menu_click_4() {
         pc_menu_list_three.style.display = 'none';
         pc_menu_list_four.style.display = 'flex';
     }
+}
+// This is closed outside click 
+// menu list 는 항상 처음에 뜨지 않도록 css에서 설정하였기에 첫화면에서는 보이지 않는다
+document.addEventListener('click', event=>{
+    //console.log('clicked');
+    const pc_menu = document.querySelector('.pc_menu');
+    // hidden 이 한번 먹히면 hidden 이 계속 먹혀버리기 때문에, 매번 한번 사용후 hidden 을 없애준다는 함수를 넣어 반복시킨다.
+    pc_menu.classList.remove('hidden');
+
+    // 최하위 선택자를 호출하여 클릭 영역이 최소로 되도록한다.
+    // closest 의 부모를 안부르고 자기자신을 호출해서 필요가 없는데 바꾸지를 못하고 있네 ? ^^
+    if(event.target.closest('.test')) return;
+    // return 이란 이전까지 작동하고 여기서 중단! 시키는것
+
+    pc_menu.classList.add('hidden');
+});
+
+// this is Mobile_menu_list
+const moreIcon_1 = document.querySelector("#moreIcon_1");
+const moreIcon_2 = document.querySelector("#moreIcon_2");
+const moreIcon_3 = document.querySelector("#moreIcon_3");
+const moreIcon_4 = document.querySelector("#moreIcon_4");
+
+let one_mobile = document.querySelector("#one_mobile");
+let two_mobile = document.querySelector("#two_mobile");
+let three_mobile = document.querySelector("#three_mobile");
+let four_mobile = document.querySelector("#four_mobile");
+
+function mobile_menu_click_1() {
+    one_mobile.classList.toggle("show_mobile_menu_list");
+    moreIcon_1.classList.toggle('lessIcon');
+}
+function mobile_menu_click_2() {
+    two_mobile.classList.toggle("show_mobile_menu_list");
+    moreIcon_2.classList.toggle('lessIcon');
+}
+function mobile_menu_click_3() {
+    three_mobile.classList.toggle("show_mobile_menu_list");
+    moreIcon_3.classList.toggle('lessIcon');
+}
+function mobile_menu_click_4() {
+   four_mobile.classList.toggle("show_mobile_menu_list");
+   moreIcon_4.classList.toggle('lessIcon');
 }
